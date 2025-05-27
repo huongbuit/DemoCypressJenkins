91785fed97f9b11e836a7e2fd9cc4e96ca54fce4
huongbuit <nttest122000@gmail.com>
2025-05-27 11:17:52 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,15 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here if needed
     },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
   },
-  reporter: "mochawesome",
+  reporter: 'mochawesome',
   reporterOptions: {
-    reportDir: 'cypress/reports/raw',   // Raw JSON reports go here
+    reportDir: 'cypress/reports/raw',
     overwrite: false,
     html: false,
     json: true
   }
-
 });
